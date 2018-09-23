6a0d8d0f129c8245c8f00011e5e55b8c9843e12e
Gorka Llona <gllona@gmail.com>
2018-09-22 20:55:26 -0500

    minor fix

--- bizmodel/BizModelAdapter.php
+++ bizmodel/BizModelAdapter.php
@@ -694,10 +694,11 @@ class BizModelAdapter extends BizModelAdapterTemplate
         exec($cmd, $stdout, $ec);
 
         $a = '';
+        $base = implode("\n", $stdout);
         if ($ec === 0) {
             $notFound = "Sorry, couldn't find any help with that topic";
-            if (substr($stdout, 0, strlen($notFound)) != $notFound) {
-                $a = '[[[' . html_entity_decode(preg_replace("/\n\n\n+/", "\n\n", trim(implode("\n", $stdout)))) . ']]]';
+            if (substr($base, 0, strlen($notFound)) != $notFound) {
+                $a = '[[[' . html_entity_decode(preg_replace("/\n\n\n+/", "\n\n", trim($base))) . ']]]';
             }
         }
 
